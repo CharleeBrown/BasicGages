1edaa6a41564a55886e610fb1c2c133bc63209ef
Coleman <charleebrown22@gmail.com>
2023-02-28 09:20:00 -0600

    Switched to MainForm until I fix the Login issues

--- BasicGages/Program.cs
+++ BasicGages/Program.cs
@@ -11,7 +11,7 @@ namespace BasicGages
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new LoginForm());
+            Application.Run(new MainForm());
         }
     }
 }
